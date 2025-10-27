ce70558701da90310c79694a903c74fff892f4f6
eb8dev <bhanuprakasheenadula12@outlook.com>
2025-10-27 23:56:52 +0530

    fixed errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import logo from "./assets/logo-circle.png";
 import "./App.css";
 import Menu from "./Menu";
--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./Menu.css";
 
 const menuItems = [
